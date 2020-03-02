804456ee85aef79ffe80d109d49ff670021a705c
vidura <vidura.hemawansa@pyxle.io>
2020-03-02 13:37:30 +0530

    added changes

--- js/scripts.js
+++ js/scripts.js
@@ -21,3 +21,9 @@ $(".apparal-select-wrapper .img-wrapper img").click(function(){
 	$(".apparal-select-wrapper .img-wrapper").removeClass("active");
 	$(this).parent().addClass("active");
 });
+
+$(".size-change-wrapper input[name='sizes']").change(function(){
+	var changedSize = $(this).val();
+	$(".top-image-rotate img").removeClass("small medium large");
+	$(".top-image-rotate img").addClass(changedSize);
+});
